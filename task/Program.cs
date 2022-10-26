b27b72db7fe12c2a023800f41aeca5beae098733
Anastasia <egorova_nastua@mail.ru>
2022-10-26 17:53:30 +0700

    add task to github

---
+++ task/Program.cs
@@ -0,0 +1,56 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует
+// массив из строк, длина которых маньше либо равна 3 символа.
+
+
+Console.WriteLine("Введите длину массива");
+int num = Convert.ToInt32(Console.ReadLine());
+string[] handArray = new string[num];
+FillStringArray(handArray);
+
+Console.Write("[" + string.Join(", ", handArray) + "]");
+Console.WriteLine();
+
+int numLen = 3; 
+int NewArrayLen = CountNewArrayLenght(handArray, numLen);
+Console.WriteLine(NewArrayLen);
+
+if (NewArrayLen == 0) Console.WriteLine("[]");
+else
+{
+    string[] newArray = new string[NewArrayLen];
+    FillNewStringArray(handArray, newArray, numLen);
+    Console.Write("[" + string.Join(", ", newArray) + "]");
+    Console.WriteLine();
+}
+
+void FillStringArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} значение массива");
+        array[i] = Console.ReadLine();
+    }
+}
+
+int CountNewArrayLenght(string[] arr, int numLenght)
+{
+    int counter = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= numLenght) counter++;
+    }
+    return counter;
+}
+
+void FillNewStringArray(string[] arrayOld, string[] arrayNew, int numStringLen)
+{
+    int newArrayIndex = 0;
+    for (int i = 0; i < arrayOld.Length; i++)
+    {
+        if (arrayOld[i].Length <= numStringLen)
+        {
+            arrayNew[newArrayIndex] = arrayOld[i];
+            newArrayIndex++;
+        }
+    }
+}
